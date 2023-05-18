f5f3430682229463aea94172513f647a122299f2
Vladimir <vnb.kh.vnb@gmail.com>
2023-05-18 20:24:33 +0300

    add компонент

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,42 @@ import ReactDOM from 'react-dom/client'
 const title = React.createElement('h1', { id: 'title' }, 'hellow react')
 console.log(title)
 // jsx
-const app = (
-    <div>
-        <h1 id="text">Hello App</h1>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
-            nobis et in fugit sequi natus, iure dicta aliquam rem, eaque qui
-            minima molestias fuga voluptatum doloremque. Veniam minima ducimus
-            in.
-        </p>
-    </div>
-)
+// const app = (
+//     <div>
+//         <h1 id="text">Hello App</h1>
+//         <p>
+//             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+//             nobis et in fugit sequi natus, iure dicta aliquam rem, eaque qui
+//             minima molestias fuga voluptatum doloremque. Veniam minima ducimus
+//             in.
+//         </p>
+//     </div>
+// )
+const App = () => {
+    return (
+        <div>
+            <h1>Hello App component</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Tempore facilis aperiam id, omnis voluptatem laboriosam quo ex
+                porro magni alias eum earum. At, est dolorem odit eaque nam
+                voluptates deserunt.
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Tempore facilis aperiam id, omnis voluptatem laboriosam quo ex
+                porro magni alias eum earum. At, est dolorem odit eaque nam
+                voluptates deserunt.
+            </p>
+            
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        {' '}
+        <App />{' '}
+    </React.StrictMode>
+)
