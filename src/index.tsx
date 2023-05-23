b47982677837b433938b616376b5c5a2a0f7e0ff
Vladimir <vnb.kh.vnb@gmail.com>
2023-05-23 19:46:04 +0300

    Добавили дефолтний експорт в окремий фал винесли

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
+import Title from './Title'
 
-
-type TitleProps = {
-    title :string
-    num?:number
-}
-
-
-
-
-const Title =  ({title,num = 5}: TitleProps) => {
-    return <h1>Hello {title} {num}</h1>
-}
 const Content = () => {
     return (
         <React.Fragment>
-            <p className='red'>
+            <p className="red">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Tempore facilis aperiam id, omnis voluptatem laboriosam quo ex
                 porro magni alias eum earum. At, est dolorem odit eaque nam
@@ -35,8 +24,8 @@ const Content = () => {
 const App = () => {
     return (
         <>
-            <Title  title='App' num={10}  />
-            <Title title='Vov' />
+            <Title title="App" num={10} />
+            <Title title="Vov" />
             <Content />
         </>
     )
