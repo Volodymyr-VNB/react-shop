c1114c6059f7aff7c3d854c15d363d13863bc037
Vladimir <vnb.kh.vnb@gmail.com>
2023-05-23 20:23:35 +0300

    винесли APP  та зробили відносні посиланн

---
+++ src/Container/App/App.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+    return  <>
+    
+    <h1>heloy app</h1>
+    </>
+    
+}
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style.css'
-import Title from './Title'
-import { Content } from './Content'
+import App from 'Container/App/App'
 
-const App = () => {
-    return (
-        <>
-            <Title title="App" num={10} />
-            <Title title="Vov" />
-            <Content text1='123132' text2='456456456' />
-        </>
-    )
-}
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
