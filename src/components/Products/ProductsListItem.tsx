9bfebeee03e1a7017eeda2eea937a5bd419dd3f6
Vladimir <vnb.kh.vnb@gmail.com>
2023-06-01 20:10:09 +0300

    класичний вариант класового компонента

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -1,38 +1,77 @@
-import { Button, Card, CardActions, CardContent, TextField } from "@mui/material"
-import "./ProductsListItem.scss"
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    TextField,
+} from '@mui/material'
+import './ProductsListItem.scss'
+import React from 'react'
 
 type Props = {
-  title: string
+    title: string
     description: string
     capacity: string
     type: string
     price: number
     image: string
-
 }
-const ProductsListItem = ({title,description,type,capacity,price,image}: Props) => {
-  return (
-    <Card className="product" variant="outlined">
-        <CardContent>
-          <div className="product-img">
-            <img src={image} alt="" />
-          </div>
-                <h4 className="product-title">{title}</h4>
-                <p className="product-description">{description}</p>
-                <div className="product-features">Type:{type}</div>
-                <div className="product-features">Capacity:{capacity}</div>
-                <div className="product-price">Price: ${price}</div>
-                <div className="product-quantity">
-                  <Button  variant="outlined">-</Button>
+class ProductsListItem extends React.Component <Props>{
+  // Це класовий компонент 
+  render() {
+    return (
+      <Card className="product" variant="outlined">
+          <CardContent>
+              <div className="product-img">
+                  <img src={this.props.image} alt="" />
+              </div>
+              <h4 className="product-title">{this.props.title}</h4>
+              <p className="product-description">{this.props.description}</p>
+              <div className="product-features">Type:{this.props.type}</div>
+              <div className="product-features">Capacity:{this.props.capacity}</div>
+              <div className="product-price">Price: ${this.props.price}</div>
+              <div className="product-quantity">
+                  <Button variant="outlined">-</Button>
                   <TextField value={1} size="small"></TextField>
-                  <Button  variant="outlined">+</Button>
-                </div>
-            <CardActions className="bnt-wrap">
-                <Button variant="outlined"> Add to cart  </Button>
-            </CardActions>
-
-        </CardContent>
-    </Card>
+                  <Button variant="outlined">+</Button>
+              </div>
+              <CardActions className="bnt-wrap">
+                  <Button variant="outlined"> Add to cart </Button>
+              </CardActions>
+          </CardContent>
+      </Card>
   )
+  }
 }
+// const ProductsListItem = ({
+//     title,
+//     description,
+//     type,
+//     capacity,
+//     price,
+//     image,
+// }: Props) => {
+//     // return (
+//     //     <Card className="product" variant="outlined">
+//     //         <CardContent>
+//     //             <div className="product-img">
+//     //                 <img src={image} alt="" />
+//     //             </div>
+//     //             <h4 className="product-title">{title}</h4>
+//     //             <p className="product-description">{description}</p>
+//     //             <div className="product-features">Type:{type}</div>
+//     //             <div className="product-features">Capacity:{capacity}</div>
+//     //             <div className="product-price">Price: ${price}</div>
+//     //             <div className="product-quantity">
+//     //                 <Button variant="outlined">-</Button>
+//     //                 <TextField value={1} size="small"></TextField>
+//     //                 <Button variant="outlined">+</Button>
+//     //             </div>
+//     //             <CardActions className="bnt-wrap">
+//     //                 <Button variant="outlined"> Add to cart </Button>
+//     //             </CardActions>
+//     //         </CardContent>
+//     //     </Card>
+//     // )
+// }
 export default ProductsListItem
