074cd42c58f7700e486b13b528ebf568540716f1
Vladimir <vnb.kh.vnb@gmail.com>
2023-06-15 18:41:43 +0300

    дз не зробив

--- src/components/CartProductList/CartProductListItemExtended.tsx
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -1,12 +1,37 @@
 import { Product } from 'utils/productArray'
 import { Card, Grid, CardContent, Button } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
+import { useState } from 'react'
 
 type Props = {
     product: Product
     productCount: number
 }
 const CartProductListItemExtended = ({ product, productCount }: Props) => {
+const  [employee, setEmployee] = useState(product);
+
+
+const removeKey = () => {
+    setEmployee(current => {
+      // 👇️ create copy of state object
+      const korzina = Object.assign({}, current);
+    //   const {korzina, ...korzina} = current;
+      // 👇️ remove salary key from object
+        // delete Object.keys.korzina[2];
+         const invalue = Object.values(current)[0];
+         console.log(current);
+
+         
+                //   delete korzina[current.id];
+                  console.log(current);   
+           
+                  
+                  
+//  delete korzina[{invalue}];
+      return employee;
+    });
+  };
+
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Card>
@@ -21,7 +46,7 @@ const CartProductListItemExtended = ({ product, productCount }: Props) => {
                     <div className="product-festures">
                         Count:{productCount}{' '}
                     </div>
-                    <Button variant="outlined">
+                    <Button variant="outlined" onClick={removeKey}>
                         <DeleteIcon />
                     </Button>
                 </CardContent>
